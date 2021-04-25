52e49434880086acef648951c8a5310c0c79be64
Daniel Porto <danielporto@MacBook-Air-de-Daniel.local>
2021-04-25 12:11:57 -0300

    action types improved

--- src/state/reducers/npmpackagesReducer.ts
+++ src/state/reducers/npmpackagesReducer.ts
@@ -1,21 +1,34 @@
-interface NPMpackagesState {
+interface NPMPackagesState {
   loading: boolean;
   error: string | null;
   data: string[];
 }
 
-interface Action {
-  type: string;
-  payload?: any;
+interface SearchNPMPackagesAction {
+  type: 'search_npm_packages';
+}
+interface SearchNPMPackagesSuccessAction {
+  type: 'search_npm_packages_success';
+  payload: string[];
+}
+interface SearchNPMPackagesErrorAction {
+  type: 'search_npm_packages_error';
+  payload: string;
 }
 
-const reducer = (state: NPMpackagesState, action: Action): NPMpackagesState => {
+const reducer = (
+  state: NPMPackagesState,
+  action:
+    | SearchNPMPackagesAction
+    | SearchNPMPackagesSuccessAction
+    | SearchNPMPackagesErrorAction
+): NPMPackagesState => {
   switch (action.type) {
-    case 'search_npmpackages':
+    case 'search_npm_packages':
       return { loading: true, error: null, data: [] };
-    case 'search_npmpackages_success':
+    case 'search_npm_packages_success':
       return { loading: false, error: null, data: action.payload };
-    case 'search_npmpackages_error':
+    case 'search_npm_packages_error':
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;
